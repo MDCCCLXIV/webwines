8b32b673464564eaecd3973822121ea1b9ec5bdd
MDCCCLXIV <ibrahimnyagesuka@gmail.com>
2023-07-14 19:42:04 +0300

    added hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='container mx-auto grid grid-cols-6 gap-2  m-1 h-full p-2'>
+    <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+      <div className='col-span-5 grid grid-cols-5 grid-rows-3 grid-flow-row gap-2 p-2 overflow-y-scroll '>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+        <div className=' bg-slate-50 shadow-md rounded'>hello</div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Hero from '../components/Hero'
 
 const Home = () => {
   return (
-    <div className='flex container flex-col justify-items-center w-full h-screen mx-auto sm:my-2'>
+    <div className='flex container flex-col justify-items-center w-[70%] h-screen mx-auto sm:my-2'>
         <Navbar />
         <Hero />
     </div>
